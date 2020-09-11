d4bf4f2b5f724793b112292edeb8d427cec28b92
polokoti <63522828+polokoti@users.noreply.github.com>
2020-09-11 10:57:31 +0700

    Bird, Catapult, and GameController

---
+++ Angry Bird Like/Assets/Scripts/GameController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public SlingShooter SlingShooter;
+    public List<Bird> Birds;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SlingShooter.InitiateBird(Birds[0]);
+    }
+
+}
